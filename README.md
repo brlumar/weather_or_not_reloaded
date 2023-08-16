29c4968ab7e0033181c3feeb9705cdb8744b84ac
brlumar <126432304+brlumar@users.noreply.github.com>
2023-08-16 01:51:34 -0500

    Final working site

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # weather_or_not
+
+Certainly! Here's a README file for your HTML project:
+
+# WeatherOrNot
+
+Welcome to **WeatherOrNot**, your ultimate source for travel weather information. Plan your trips wisely and stay informed about the weather conditions using this web application.
+
+## Features
+
+- **Current Weather:** Instantly access real-time weather updates for your desired location.
+- **5-Day Forecast:** Plan your week ahead with a 5-day weather forecast.
+- **Location Detection:** Utilize GPS to get weather information for your current location.
+- **Search:** Search for weather details by entering a city name or zip code.
+- **History:** Keep track of your search history for easy reference.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies](#technologies)
+- [Contributing](#contributing)
+- [License](#license)
+
+
+## Usage
+
+1. Upon opening the application, you'll be greeted with the main page showcasing the title and subtitle.
+2. Click the GPS icon to fetch weather information based on your current location.
+3. Alternatively, you can manually enter a city name or zip code in the input box and click the "SEARCH" button.
+4. The main section of the page will display the current weather information, including temperature, wind speed, and humidity.
+5. Scroll down to view the 5-day weather forecast, providing essential details for each day.
+
+## Technologies
+
+- HTML5
+- CSS3 (Bulma framework)
+- JavaScript (jQuery and Day.js)
+- OpenWeatherMap API
+
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Developed by Brandon Lumar (https://github.com/brlumar) | 
+
+
+
+---
+
+Stay weather-prepared with WeatherOrNot! 🌦️🌧️🌞
+
+![screen-shot of website ](image.png)
+
--- js/script.js
+++ js/script.js
@@ -7,16 +7,16 @@ let cityInput = $('#input-box'); //captures the city entered in the input box
 let cityName = '';
 let zipCode = '';
 
+let printCity = document.getElementById('print-city-history');
 const todaysWeatherEL = document.getElementById('todays-container'); //points to today's weather in the DOM
 const forecastEL = document.getElementById('five-day'); //points to the forecast in the DOM
 let textArray = []; //array for local data
-
 
 var currentHour = dayjs().hour(); //gets the current hour
 
 const apiKey = 'd76c91059df9d7069e6671bf4641cd06'; //api key for open weather map api
 let newCityName = ''; //holds the city name pulled from the api objust after data is retrieved
-let cityHistory=[]; //holds the array of past cities
+let cityHistory = []; //holds the array of past cities
 //Function that sets the current time. 
 setInterval(function () {
     var dateInterval = dayjs(); //js constructor to create a new date object.
@@ -24,26 +24,76 @@ setInterval(function () {
 }, 1000);
 
 
-function addInputToArray(){}
+function addInputToArray() {
+
+    textArray[textArray.length] = newCityName;
+    console.log(' the city array is :', textArray);
+
+}
+
+function printToHistory() {
+    // Clear the existing buttons
+    printCity.innerHTML = '';
+    textArray.forEach(cityInArray =>{
+        console.log('printing ', cityInArray);
+        // printCity.
+
+        const button = document.createElement('button'); //creates a button
+       
+        button.innerText = cityInArray; //changes the inner text of the button
+        button.classList.add('past-city');
+        button.classList.add('button');
+        button.classList.add('is-info');
+        button.classList.add('is-rounded');
+        button.classList.add('is-small');
+        button.addEventListener('click', function() {
+            getWeatherDataHistory(button.innerText)
+        });
+
+
+       
+
+        // button.addEventListener('click', () => selectAnswer(answer.correct));
+        printCity.appendChild(button);
+        // printCity += `<button class="past-city button is-info is-rounded is-small">${cityInArray}</button>`
+    saveUserData(textArray);
+    } 
+    ) 
+}
+
+//function that converts the array into a JSON value so it can be used in local storage
+function saveUserData(arr) {
+    var jasonVal = JSON.stringify(arr);
+    localStorage.setItem('cities', jasonVal);
+}
+
+//function that gets the JSON array from the local storage and converts it to a JavaScript array
+function addToArray() {
+    var rawData = localStorage.getItem('cities');
+    var parse = JSON.parse(rawData) || [];
+
+    return parse;
+}
+
 
 function setWeatherData(data) {
 
 
     let todayForecast = '';
     let nextDayForecast = '';
-    // data.list.array.forEach(element => {
     newCityName = data.city.name;
 
     data.list.forEach((day, idx) => {
-        if (idx == 0) {
+        if (idx == 2) {
             console.log('We are in the loop', idx);
+            let timestamp = day.dt*1000;
 
             todayForecast += `
             <div class="todays-container" id="todays-temp">
-            <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" id="today-icon" class="w-icon">
+            <img src="https://openweathermap.org/img/wn/10d@4x.png" alt="weather icon" id="today-icon" class="w-icon">
             <div id="current-city">${newCityName}</div>
             <div class="today-info">
-                <div class="day">Monday</div>
+                <div class="day">${dayjs(timestamp).format('dddd')}</div>
                 <div class="temp">Temp:${day.main.temp}F</div>
                 <div class="wind">Wind ${day.wind.speed} MPH</div>
                 <div class="humidity">Humidity ${day.main.humidity}%</div>
@@ -54,18 +104,23 @@ function setWeatherData(data) {
 
 
 
-        } else if (idx <= 5) {
+        } else if (idx <= 27) {
             console.log('We are in the next loop', idx);
+            let timestamp = day.dt*1000;
+            console.log('today is :', dayjs(timestamp));
+           
+
+            if(idx==7||idx==12||idx==17||idx==22||idx==27){
             nextDayForecast += `
             
             <div id="day1" class = "column forecast-containers">
             <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">  
-            <div class="day">Sat</div>
+            <div class="day">${dayjs(timestamp).format('dddd')}</div>
             <div class="temp">Temp: ${day.main.temp}F</div>
             <div class="wind">Wind ${day.wind.speed} MPH</div>
             <div class="humidity">Humid: ${day.main.humidity}%</div>
         </div >`
-            console.log(day.main.temp);
+            console.log(day.main.temp);}
         }
 
 
@@ -88,7 +143,7 @@ $(inputButton).on('click', function () {
         console.log("thats a city son!")
         getWeatherDataCity();
         console.log('input entered');
-    } else if (checker === 'Zip Code'){
+    } else if (checker === 'Zip Code') {
         getWeatherDataZip();
         console.log("that's a zip code kid");
     }
@@ -118,27 +173,33 @@ function getWeatherDataZip() { //function that gets weather data from open weath
     fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${cityName}&appid=${apiKey}&units=imperial`).then(res => res.json()).then(data => {
         console.log(data)
         setWeatherData(data);
+        addInputToArray();
+        printToHistory();
 
     })
 }
 
-// function clearInput(){
-//     cityInput.
-// }
 
 function getWeatherDataCity() { //function that gets weather data from open weather man api
 
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`).then(res => res.json()).then(data => {
         console.log(data)
         setWeatherData(data);
-
+        addInputToArray();
+        printToHistory();
     })
 }
 
-// function getLatLog(city){
+function getWeatherDataHistory(repeatCity) { //function that gets weather data from open weather man api
 
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${repeatCity}&appid=${apiKey}&units=imperial`).then(res => res.json()).then(data => {
+        console.log(data)
+        setWeatherData(data);
+        addInputToArray();
+        printToHistory();
+    })
+}
 
-// }
 
 
 function getWeatherData() { //function that gets weather data from open weather man api
@@ -148,10 +209,15 @@ function getWeatherData() { //function that gets weather data from open weather 
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`).then(res => res.json()).then(data => {
             console.log(data)
             setWeatherData(data);
-            
+            addInputToArray();
+            printToHistory();
         })
 
     })
 }
 
 
+
+//Runs when page loads to get past cities from history and create buttons for them
+textArray =addToArray();
+printToHistory();
